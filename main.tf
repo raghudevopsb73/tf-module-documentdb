0ba7ddb70ada2a8d9351057471a86434d4b01230
Raghu K <raghuk.vit@gmail.com>
2023-06-26 08:27:05 +0530

    Update tf-module-documentdb/

--- main.tf
+++ main.tf
@@ -4,13 +4,47 @@ resource "aws_docdb_subnet_group" "main" {
   tags       = merge({ Name = "${var.component}-${var.env}" }, var.tags)
 }
 
-resource "aws_docdb_cluster" "main" {
-  cluster_identifier      = "${var.component}-${var.env}"
-  engine                  = var.engine
-  master_username         = "foo"
-  master_password         = "mustbeeightchars"
-  backup_retention_period = 5
-  preferred_backup_window = "07:00-09:00"
-  skip_final_snapshot     = true
+resource "aws_security_group" "main" {
+  name        = "${var.component}-${var.env}-sg"
+  description = "${var.component}-${var.env}-sg"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = var.port
+    to_port     = var.port
+    protocol    = "tcp"
+    cidr_blocks = var.sg_subnet_cidr
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.component}-${var.env}-sg"
+  }
 }
+
+resource "aws_docdb_cluster" "main" {
+  cluster_identifier     = "${var.component}-${var.env}"
+  engine                 = var.engine
+  engine_version         = var.engine_version
+  master_username        = data.aws_ssm_parameter.username.value
+  master_password        = data.aws_ssm_parameter.password.value
+  skip_final_snapshot    = true
+  db_subnet_group_name   = aws_docdb_subnet_group.main.name
+  vpc_security_group_ids = [aws_security_group.main.id]
+  kms_key_id             = var.kms_key_arn
+}
+
+resource "aws_docdb_cluster_instance" "main" {
+  count              = var.db_instance_count
+  identifier         = "${var.component}-${var.env}-${count.index}"
+  cluster_identifier = aws_docdb_cluster.main.id
+  instance_class     = var.instance_class
+}
+
 
--- variables.tf
+++ variables.tf
@@ -4,3 +4,11 @@ variable "tags" {}
 variable "subnet_ids" {}
 variable "sg_subnet_cidr" {}
 variable "kms_key_arn" {}
+variable "engine_version" {}
+variable "engine" {}
+variable "port" {
+  default = 27017
+}
+variable "vpc_id" {}
+variable "db_instance_count" {}
+variable "instance_class" {}
