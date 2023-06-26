41006653d8fcb4ae37c83c68e5fc6f493ebf2e9b
Raghu K <raghuk.vit@gmail.com>
2023-06-26 07:57:37 +0530

    Update tf-module-documentdb/

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_docdb_subnet_group" "default" {
+resource "aws_docdb_subnet_group" "main" {
   name       = "${var.component}-${var.env}"
   subnet_ids = var.subnet_ids
   tags       = merge({ Name = "${var.component}-${var.env}" }, var.tags)
