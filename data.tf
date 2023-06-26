53a3237de73e2b9abda17bf7663f13aa05d05df7
Raghu K <raghuk.vit@gmail.com>
2023-06-26 08:17:09 +0530

    Update tf-module-documentdb/

---
+++ data.tf
@@ -0,0 +1,8 @@
+data "aws_ssm_parameter" "username" {
+  name = "roboshop.${var.env}.docdb.username"
+}
+
+data "aws_ssm_parameter" "password" {
+  name = "roboshop.${var.env}.docdb.password"
+}
+
--- main.tf
+++ main.tf
@@ -4,3 +4,13 @@ resource "aws_docdb_subnet_group" "main" {
   tags       = merge({ Name = "${var.component}-${var.env}" }, var.tags)
 }
 
+resource "aws_docdb_cluster" "main" {
+  cluster_identifier      = "${var.component}-${var.env}"
+  engine                  = var.engine
+  master_username         = "foo"
+  master_password         = "mustbeeightchars"
+  backup_retention_period = 5
+  preferred_backup_window = "07:00-09:00"
+  skip_final_snapshot     = true
+}
+
